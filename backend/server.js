5c4e49f266e2051b6531a093c6d4183928c30394
Yash Uppal <2k22.cse.2213230@gmail.com>
2025-12-02 16:01:18 +0530

    fix: use CommonJS uuid import for Vercel

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid/dist/v4');  
 
 const app = express();
 const server = http.createServer(app);
